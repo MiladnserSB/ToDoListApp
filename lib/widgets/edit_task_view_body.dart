c60d3a7fd006f24cbd559486dae3c5194fca422b
Milad <Milad@gmail.com>
2024-09-26 03:06:15 +0300

    finish edit task view UI

--- lib/widgets/edit_task_view_body.dart
+++ lib/widgets/edit_task_view_body.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list_app/widgets/custom_app_bar.dart';
+import 'package:to_do_list_app/widgets/custom_button.dart';
+import 'package:to_do_list_app/widgets/custom_text_field.dart';
 
 class EditTaskViewBody extends StatelessWidget {
   const EditTaskViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return  Column(
       children: [
         
-        CustomAppBar(title: 'title')
+       const CustomAppBar(title: 'Edit Task'),
+       const SizedBox(
+            height: 35,
+          ),
+       const  Padding(
+           padding:  EdgeInsets.symmetric(horizontal:  25.0),
+           child:  CustomTextField(
+              hint: 'Title',
+              maxLines: 5,
+            ),
+         ),
+          const SizedBox(
+            height: 35,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:  25.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: CustomButton(
+                      title: 'Edit',
+                      onTap: () {
+                        // print('object');
+                      }),
+                ),
+                    SizedBox(width: 20,),
+                Expanded(
+                  child: CustomButton(
+                      title: 'Back',
+                      onTap: () {
+                        // print('object');
+                      }),
+                ),
+              ],
+            ),
+          )
       ],
     );
   }
